1c4864a2e4a1c99291e3f8100265bac20510e33f
RICCOM <ericomun@gmail.comgit>
2024-03-23 17:18:07 +0300

    add arrow-function

---
+++ index.js
@@ -0,0 +1,12 @@
+//divide function
+const divide = (a, b) => {
+    return a / b;
+  };
+//square function
+  const square = (x) => {
+    return x * x;
+  };
+  //add function
+  const add = (a, b) => {
+    return a + b;
+  };
